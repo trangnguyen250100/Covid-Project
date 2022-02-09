0adf3d12ab4ec01a33447ca44f5156145f6222fd
Trang Nguyễn <92616744+trangnguyen250100@users.noreply.github.com>
2022-02-09 15:01:36 +0700

    Update SQL_COVID_EXPLORATION.sql

--- SQL_COVID_EXPLORATION.sql
+++ SQL_COVID_EXPLORATION.sql
@@ -1,16 +1,18 @@
 -- THIS IS THE COVID DATASET
 
-set sql_safe_updates = 0; 
+set sql_safe_up
+
+s = 0; 
 
 -- IMPORT DATA FROM CSV FILES
 
 create table covid_vaccination 
 ( 
-continent text, 
-location text, 
+continent char(255), 
+location char(255), 
 date text,
-total_vaccinations text(11), 
-new_vaccinations text(11), 
+total_vaccinations text, 
+new_vaccinations text, 
 population text
 ); 
 
@@ -23,8 +25,8 @@ ignore 1 rows;
 
 create table covid_death
 ( 
-continent text, 
-location text, 
+continent char(255), 
+location char(255), 
 date text,
 total_cases text, 
 new_cases text, 
@@ -56,143 +58,28 @@ set date = cast(date as date);
 -- If it doesn't update properly, add converted columns
 
 alter table covid_death
-add continent_converted char(255), 
-add location_converted char(255), 
 add date_converted date, 
-add total_cases_converted int, 
-add new_cases_converted int,
-add total_deaths_converted int, 
-add new_deaths_converted int, 
-add population_converted int;
 
 update covid_death
-set continent_converted = cast(continent as char(255));  
-
-update covid_death
-set location_converted = cast(location as char(255)); 
-
-update covid_death
-set date_converted = str_to_date(date, '%d %m %Y'); 
-
-update ignore covid_death
-set total_cases_converted = cast(total_cases as signed); 
-
-update ignore covid_death
-set new_cases_converted = cast(new_cases as signed); 
-
-update ignore covid_death
-set total_deaths_converted = cast(total_deaths as signed); 
-
-update ignore covid_death
-set new_deaths_converted = cast(new_deaths as signed); 
-
-update ignore covid_death 
-set population_converted = cast(population as signed); 
+set date_converted = str_to_date(date, '%d %m %Y'),
+total_cases = cast(total_cases as signed),
+new_cases = cast(new_cases as signed),
+total_deaths = cast(total_deaths as signed),
+new_deaths = cast(new_deaths as signed),
+population = cast(population as signed); 
 
 select * from covid_death; 
 
 -- Covid vaccinations table 
 
 alter table covid_vaccination
-add continent_converted char(255), 
-add location_converted char(255), 
 add date_converted date,
-add total_vaccinations_converted int,
-add new_vaccinations_converted int,
-add population_converted int; 
 
 update covid_vaccination
-set continent_converted = cast(continent as char(255));
-
-update covid_vaccination
-set location_converted = cast(location as char(255)); 
-
-update covid_vaccination
-set date_converted = str_to_date(date, '%d %m %Y'); 
-
-update ignore covid_vaccination
-set total_vaccinations_converted = cast(total_vaccinations as signed); 
-
-update ignore covid_vaccination
-set new_vaccinations_converted = cast(new_vaccinations as signed);
-
-update ignore covid_vaccination
-set population_converted = cast(population as signed); 
-
-select * 
-from covid_vaccination; 
-
----------------------------------------------------------------------------------------------------
-
--- DELETE UNUSED COLUMNS & RENAME THE CONVERTED COLUMNS
-
--- Covid deaths table 
-
-alter table covid_death
-drop column continent;
-
-alter table covid_death
-drop column location; 
-
-alter table covid_death
-drop column date; 
-
-alter table covid_death
-drop column total_cases; 
-
-alter table covid_death
-drop column new_cases; 
-
-alter table covid_death
-drop column total_deaths; 
-
-alter table covid_death
-drop column new_deaths; 
-
-alter table covid_death
-drop column population; 
-
-
-alter table covid_death
-change `continent_converted` `continent` char(255),
-change `location_converted` `location` char(255), 
-change `date_converted` `date` date,
-change `total_cases_converted` `total_cases` int,
-change `new_cases_converted` `new_cases` int, 
-change `total_deaths_converted` `total_deaths` int, 
-change `new_deaths_converted` `new_deaths` int,
-change `population_converted` `population` int; 
-
-select * 
-from covid_death; 
-
--- Covid vaccinations table 
-
-alter table covid_vaccination
-drop column continent; 
-
-alter table covid_vaccination
-drop column location; 
-
-alter table covid_vaccination
-drop column date; 
-
-alter table covid_vaccination
-drop column total_vaccinations; 
-
-alter table covid_vaccination
-drop column new_vaccinations; 
-
-alter table covid_vaccination
-drop column population;
-
-alter table covid_vaccination
-change `continent_converted` `continent` char(255), 
-change `location_converted` `location` char(255),
-change `date_converted` `date` date, 
-change `total_vaccinations_converted` `total_vaccinations` int, 
-change `new_vaccinations_converted` `new_vaccinations` int,
-change `population_converted` `population` int; 
+set date_converted = str_to_date(date, '%d %m %Y'),
+total_vaccinations = cast(total_vaccinations as signed),
+new_vaccinations = cast(new_vaccinations as signed),
+population = cast(population as signed); 
 
 select * 
 from covid_vaccination; 
@@ -227,13 +114,10 @@ where continent = '';
 -- CREATE INDEX COLUMNS
 
 create index continent on covid_death (continent);
-create index location on covid_death (location); 
-create index date on covid_death (date); 
+create index name on covid_death (location, date_converted); 
 
 create index continent on covid_vaccination (continent);
-create index location on covid_vaccination (location); 
-create index date on covid_vaccination (date); 
-
+create index name on covid_vaccination (location, date_converted); 
 
 -----------------------------------------------------------------------------------------------------
 
@@ -261,20 +145,20 @@ order by total_death_count desc;
 
 -- Show the probability of dying if infected with covid-19 in each country 
 
-select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
+select location, date_converted, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
 from covid_death
 order by 1,2; 
 
 -- Show the probability of dying if infected with covid-19 in Vietnam 
 
-select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
+select location, date_converted, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
 from covid_death
 where location = 'Vietnam'
 order by 2; 
 
--- Show percentage of population infected and died by covid-19 
+-- Show percentage of population infected and died by covid-19
 
-select location, date, population, total_cases, 
+select location, date_converted, population, total_cases, 
 	(total_cases/population)*100 as percent_population_infected,
     (total_deaths/population)*100 as percent_population_died
 from covid_death
@@ -288,32 +172,32 @@ from covid_death
 group by location, population
 order by percent_population_infected desc; 
 
-select location, population, date, max(total_cases) as total_infection_count, 
+select location, population, date_converted, max(total_cases) as total_infection_count, 
 	(max(total_cases)/population)*100 as percent_population_infected
 from covid_death
-group by location, population, date
+group by location, population, date_converted
 order by percent_population_infected desc; 
 
 -- Percentage of population in each country that has received at least one covid vaccine 
 
-select d.continent, d.location, d.date, d.population, v.new_vaccinations,
+select d.continent, d.location, d.date_converted, d.population, v.new_vaccinations,
 	sum(v.new_vaccinations) over (partition by d.location) as people_vaccinated
 from covid_death d 
 join covid_vaccination v 
 	on d.location = v.location
-    and d.date = v.date
-order by d.location, d.date; 
+    and d.date_converted = v.date_converted
+order by d.location, d.date_converted; 
 
 -- Using CTE to perform calculation on partition by in previous query 
 with PopulationvsVaccinations (continent, location, date, population, new_vaccinations, rolling_people_vaccinated) 
 as
 (
-select d.continent, d.location, d.date, d.population, v.new_vaccinations,
+select d.continent, d.location, d.date_converted, d.population, v.new_vaccinations,
 	sum(v.new_vaccinations) over (partition by d.location) as people_vaccinated
 from covid_death d 
 join covid_vaccination v 
 	on d.location = v.location
-    and d.date = v.date
+    and d.date_converted = v.date_converted
 )
 select *, (people_vaccinated/population)*100
 from PopulationvsVaccinations; 
@@ -334,13 +218,13 @@ index (continent, location, date)
 ); 
 
 insert into percent_population_vaccinated
-select d.continent, d.location, d.date, d.population, v.new_vaccinations,
+select d.continent, d.location, d.date_converted, d.population, v.new_vaccinations,
 	sum(v.new_vaccinations) over (partition by d.location) as people_vaccinated
 from covid_death d 
 join covid_vaccination v 
 	on d.location = v.location
-    and d.date = v.date
-order by d.location, d.date; 
+    and d.date_converted = v.date_converted
+order by d.location, d.date_converted; 
 
 select *, (people_vaccinated/population)*100
 from percent_population_vaccinated; 
@@ -349,12 +233,12 @@ from percent_population_vaccinated;
 
 create view PercentPopulationVaccinated 
 as 
-select d.continent, d.location, d.date, d.population, v.new_vaccinations,
+select d.continent, d.location, d.date_converted, d.population, v.new_vaccinations,
 	sum(v.new_vaccinations) over (partition by d.location) as people_vaccinated
 from covid_death d 
 join covid_vaccination v 
 	on d.location = v.location
-    and d.date = v.date; 
+    and d.date_converted = v.date_converted; 
 
 select *, (people_vaccinated/population)*100
 from PercentPopulationVaccinated;
